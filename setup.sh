8cde1e89152d19f08deff054ee515b4f6c569d91
Joseph Samuels <joseph.w.samuels@icloud.com>
2022-02-23 18:44:03 -0500

    main - Work on setup script.

--- setup.sh
+++ setup.sh
@@ -5,8 +5,6 @@ read -p 'Plugin Namespace: ' NAMESPACE
 UPPER_ABBR=$(echo $NAMESPACE | sed 's/[a-z]*//g')
 LOWER_ABBR=$(tr '[:upper:]' '[:lower:]' <<< "$UPPER_ABBR")
 
-echo $LOWER_ABBR
-
 function updateCode {
   grep -rl "$1" --exclude=setup.sh * | xargs sed -i '' "s/$1/$2/g"
 }
